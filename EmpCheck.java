8ce0216f7a8a41466a37b8d21f1fdc0ea11305a2
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-13 21:04:05 +0530

    UC4

--- EmpCheck.java
+++ EmpCheck.java
@@ -1,53 +1,66 @@
 /**
  * The Employee check class
  */
-public class EmpCheck{ 
+public class EmpCheck {
 
-	private static final int IS_PRESENT=1;
-	private static final int IS_FULL_TIME=1;
-	
-	public static void main(String [] args){
+	private static final int IS_PRESENT = 1;
+
+	public static void main(final String[] args) {
 		// Calling function
 		final boolean isPresent = empCheck();
-		if(isPresent){
-			final int empWorkHrs = empWorkHours();			
+		if (isPresent) {
+			final int empWorkHrs = empWorkHours();
 			final double wage = calulateWage(20, empWorkHrs);
 			System.out.println("Employee is Present ==>");
-			System.out.println("Total Employee wage is : "+ wage);
-		}else{
+			if (empWorkHrs == 8) {
+				System.out.println("Employee is working FullTime ==>");
+			} else {
+				System.out.println("Employee is working HalfTime ==>");
+			}
+			System.out.println("Total Employee wage is : " + wage);
+		} else {
 			System.out.println("Employee is absent ==>");
 			System.out.println("Total Employee wage is : 0.0");
 		}
 	}
-	
+
 	/**
 	 * Check employee
+	 * 
 	 * @return true if employee present otherwise false.
 	 */
-	public static boolean empCheck(){		
+	public static boolean empCheck() {
 		return IS_PRESENT == Math.floor(Math.random() * 10) % 2;
 	}
 
 	/**
 	 * Check employee Working Hours
+	 * 
 	 * @return numbers of hrs the employ worked.
 	 */
-	public static int empWorkHours(){
-		if(IS_FULL_TIME == Math.floor(Math.random() * 10) % 2){
-			return 8;
-		}else{
-			return 4;
+	public static int empWorkHours() {
+		final double timing = Math.floor(Math.random() * 10) % 2;
+		final int value = (int) timing;
+
+		switch (value) {
+			case 1:
+				return 8;
+			case 0:
+				return 4;
+			default:
+				return 0;
 		}
 	}
 
 	/**
 	 * Calculate employee wage based on params.
+	 * 
 	 * @param ratePerHrs the employee rate per hours.
-	 * @param totalHrs total employee hours.
+	 * @param totalHrs   total employee hours.
 	 * @return total employee wage.
 	 */
-	public static double calulateWage(final int ratePerHrs, final int totalHrs){
-		return ratePerHrs*totalHrs;
-	} 
-	
+	public static double calulateWage(final int ratePerHrs, final int totalHrs) {
+		return ratePerHrs * totalHrs;
+	}
+
 }
