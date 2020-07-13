61e8c3daf4cc0b7cd1e99c4c5f7aa57c62651d18
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-13 20:38:34 +0530

    UC1 & UC2

---
+++ EmpCheck.java
@@ -0,0 +1,40 @@
+/**
+ * The Employee check class
+ */
+public class EmpCheck{ 
+
+	private static final int IS_PRESENT=1;
+	
+	public static void main(String [] args){
+		// Calling function
+		final boolean isPresent = empCheck();
+		if(isPresent){
+			final double wage = calulateWage(20, 8);
+			System.out.println("Employee is Present ==>");
+			System.out.println("Total Employee wage is : "+ wage);
+		}else{
+			System.out.println("Employee is absent ==>");
+			System.out.println("Total Employee wage is : 0.0");
+		}
+	}
+	
+	/**
+	 * Check employee
+	 * @return true if employee present otherwise false.
+	 */
+	public static boolean empCheck(){
+		
+		return IS_PRESENT == Math.floor(Math.random() * 10) % 2;
+	}
+
+	/**
+	 * Calculate employee wage based on params.
+	 * @param ratePerHrs the employee rate per hours.
+	 * @param totalHrs total employee hours.
+	 * @return total employee wage.
+	 */
+	public static double calulateWage(final int ratePerHrs, final int totalHrs){
+		return ratePerHrs*totalHrs;
+	} 
+	
+}
