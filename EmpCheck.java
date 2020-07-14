b6f6f64d778fcd13a3a44f095c5d5b1e0d418cb2
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-14 21:48:43 +0530

    UC8

--- EmpCheck.java
+++ EmpCheck.java
@@ -1,4 +1,6 @@
-/**
+import java.util.Scanner;
+
+/*
  * The Employee check class
  */
 public class EmpCheck {
@@ -7,38 +9,32 @@ public class EmpCheck {
 
 	public static void main(final String[] args) {
 		// Calling function
+		final Scanner sc = new Scanner(System.in);
 
-		int countDay = 0;
-		double totalWage = 0;
-		int totalHrs = 0;
-		while (countDay < 20 && totalHrs < 100) {
-			final boolean isPresent = empCheck();
-			if (isPresent) {
-				// Calling function
-				final int empWorkHrs = empWorkHours();
-				totalHrs += empWorkHrs;
-				// Calling function
-				final double wage = calulateWage(20, empWorkHrs);
-				System.out.println("Employee is Present ==>");
-				if (empWorkHrs == 8) {
-					System.out.println("Employee is working FullTime ==>");
-				} else {
-					System.out.println("Employee is working HalfTime ==>");
-				}
-				countDay++;
-				System.out.println("Total number of days Employee worked : " + countDay);
-				System.out.println("Total number of hours Employee worked : " + totalHrs);
-				totalWage += wage;
-				System.out.println("Total Employee wage is : " + totalWage);
-			} else {
-				System.out.println("Employee is absent ==>");
-				System.out.println("Total number of days Employee worked : " + countDay);
-				System.out.println("Total number of hours Employee worked : " + totalHrs);
-				System.out.println("Total Employee wage is :" + totalWage);
+		System.out.println("Enter the total number of Days Employ have to work");
+		// taking input for number of working Days
+		final int maxDays = sc.nextInt();
 
-			}
-			System.out.println();
-		}
+		System.out.println("Enter the total number of Hours Employ have to work");
+		// taking input for number of working Hours
+		final int maxHours = sc.nextInt();
+
+		System.out.println("Enter the Employ Wage for per Hour");
+		// taking input for wage per hours
+		final int perHrsWage = sc.nextInt();
+
+		System.out.println("Enter the FULL-TIME hOURS");
+		// taking input for FULL-TIME
+		final int fullTime = sc.nextInt();
+
+		System.out.println("Enter the HALF-TIME hOURS");
+		// taking input for HALF-TIME
+		final int halfTime = sc.nextInt();
+
+		// Calling function
+		empWage(maxDays, maxHours, perHrsWage, fullTime, halfTime);
+
+		sc.close();
 	}
 
 	/**
@@ -55,15 +51,15 @@ public class EmpCheck {
 	 * 
 	 * @return numbers of hrs the employ worked.
 	 */
-	public static int empWorkHours() {
+	public static int empWorkHours(int fullTime, int halfTime) {
 		final double timing = Math.floor(Math.random() * 10) % 2;
 		final int value = (int) timing;
 
 		switch (value) {
 			case 1:
-				return 8;
+				return fullTime;
 			case 0:
-				return 4;
+				return halfTime;
 			default:
 				return 0;
 		}
@@ -78,6 +74,51 @@ public class EmpCheck {
 	 */
 	public static double calulateWage(final int ratePerHrs, final int totalHrs) {
 		return ratePerHrs * totalHrs;
+	}
+
+	/**
+	 * printing the Employ wage for everyDay.
+	 * 
+	 * @param maxDays    total number of days for the Employ to Work.
+	 * @param maxHours   total number of hours for the Employ to Work.
+	 * @param perHrsWage per hours wage for the employ.
+	 * @param fullTime   number of hours employ have to work for FULL-TIME.
+	 * @param halfTime   number of hours employ have to work for HALF-TIME.
+	 */
+	public static void empWage(int maxDays, int maxHours, int perHrsWage, int fullTime, int halfTime) {
+
+		int countDay = 1;
+		int totalHrs = 0;
+		double totalWage = 0;
+
+		while (countDay <= maxDays && totalHrs <= maxHours) {
+			final boolean isPresent = empCheck();
+			if (isPresent) {
+				// Calling function
+				final int empWorkHrs = empWorkHours(fullTime, halfTime);
+				totalHrs += empWorkHrs;
+				// Calling function
+				final double wage = calulateWage(perHrsWage, empWorkHrs);
+				System.out.println("Employee is Present ==>");
+				if (empWorkHrs == 8) {
+					System.out.println("Employee is working FullTime ==>");
+				} else {
+					System.out.println("Employee is working HalfTime ==>");
+				}
+				System.out.println("Total number of days Employee worked : " + countDay);
+				System.out.println("Total number of hours Employee worked : " + totalHrs);
+				totalWage += wage;
+				System.out.println("Total Employee wage is : " + totalWage);
+			} else {
+				System.out.println("Employee is absent ==>");
+				System.out.println("Total number of days Employee worked : " + countDay);
+				System.out.println("Total number of hours Employee worked : " + totalHrs);
+				System.out.println("Total Employee wage is :" + totalWage);
+
+			}
+			countDay++;
+			System.out.println();
+		}
 	}
 
 }
