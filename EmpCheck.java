852cb50722928a6134c3369a5c84289fd759e411
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-17 00:07:48 +0530

    UC 8

--- EmpCheck.java
+++ EmpCheck.java
@@ -1,126 +1,95 @@
-import java.util.Scanner;
-
-/*
- * The Employee check class
- */
 public class EmpCheck {
 
-	private static final int IS_PRESENT = 1;
+	private static final int IS_PART_TIME = 1;
+	private static final int IS_FULL_TIME = 2;
+
+	public static int computeEmpWage(final Company company) {
+		// variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+		while (totalEmpHrs <= company.getMaxHrsInMonth() && totalWorkingDays < company.getTotalWorkingDays()) {
+			totalWorkingDays++;
+			final int value = (int) Math.floor(Math.random() * 10) % 3;
+			switch (value) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+					break;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+		}
+		return totalEmpHrs * company.getEmpRate();
+	}
 
 	public static void main(String[] args) {
-		// Calling function
-		final Scanner sc = new Scanner(System.in);
+		// For Apple
+		final Company apple = new Company("Apple", 20, 20, 100);
+		final int totalWageApple = computeEmpWage(apple);
+		System.out.println("Total Emp wage for " + apple.getName() + ": " + totalWageApple);
 
-		System.out.println("Enter the total number of Days Employ have to work");
-		// taking input for number of working Days
-		final int maxDays = sc.nextInt();
-
-		System.out.println("Enter the total number of Hours Employ have to work");
-		// taking input for number of working Hours
-		final int maxHours = sc.nextInt();
-
-		System.out.println("Enter the Employ Wage for per Hour");
-		// taking input for wage per hours
-		final int perHrsWage = sc.nextInt();
-
-		// Calling function
-		empWage(maxDays, maxHours, perHrsWage);
-
-		sc.close();
+		// For Samsung
+		final Company samsung = new Company("samsung", 22, 30, 120);
+		final int totalWageSamSung = computeEmpWage(samsung);
+		System.out.println("Total Emp wage for " + samsung.getName() + ": " + totalWageSamSung);
 	}
 
-	/**
-	 * Check employee
-	 * 
-	 * @return true if employee present otherwise false.
-	 */
-	public static boolean empCheck() {
-		return IS_PRESENT == Math.floor(Math.random() * 10) % 2;
+}
+
+class Company {
+
+	private String name;
+
+	private int empRate;
+
+	private int totalWorkingDays;
+
+	private int maxHrsInMonth;
+
+	public Company() {
 	}
 
-	/**
-	 * Check employee Working Hours
-	 * 
-	 * @return numbers of hrs the employ worked.
-	 */
-	public static int empWorkHours() {
-
-		final double timing = Math.floor(Math.random() * 10) % 2;
-		final int value = (int) timing;
-
-		switch (value) {
-			case 1:
-				return 8;
-			case 0:
-				return 4;
-			default:
-				return 0;
-		}
+	public Company(final String name, final int empRate, final int totalWorkingDays, final int maxHrsInMonth) {
+		this.name = name;
+		this.empRate = empRate;
+		this.totalWorkingDays = totalWorkingDays;
+		this.maxHrsInMonth = maxHrsInMonth;
 	}
 
-	/**
-	 * Calculate employee wage based on params.
-	 * 
-	 * @param ratePerHrs the employee rate per hours.
-	 * @param totalHrs   total employee hours.
-	 * @return total employee wage.
-	 */
-	public static double calulateWage(final int ratePerHrs, final int totalHrs) {
-		return ratePerHrs * totalHrs;
+	public void setName(final String name) {
+		this.name = name;
 	}
 
-	/**
-	 * printing the Employ wage for everyDay.
-	 * 
-	 * @param maxDays    total number of days for the Employ to Work.
-	 * @param maxHours   total number of hours for the Employ to Work.
-	 * @param perHrsWage per hours wage for the employ.
-	 */
-	public static void empWage(final int maxDays, final int maxHours, final int perHrsWage) {
-
-		int countDay = 1;
-		int totalHrs = 0;
-		double totalWage = 0;
-
-		while (countDay <= maxDays && totalHrs <= maxHours) {
-
-			final boolean isPresent = empCheck();
-
-			if (isPresent) {
-
-				// Calling function
-				final int empWorkHrs = empWorkHours();
-
-				totalHrs += empWorkHrs;
-
-				// Calling function
-				final double wage = calulateWage(perHrsWage, empWorkHrs);
-
-				System.out.println("Employee is Present ==>");
-
-				if (empWorkHrs == 8) {
-					System.out.println("Employee is working FullTime ==>");
-				} else {
-					System.out.println("Employee is working HalfTime ==>");
-				}
-
-				System.out.println("Total number of days Employee worked : " + countDay);
-				System.out.println("Total number of hours Employee worked : " + totalHrs);
-				totalWage += wage;
-				System.out.println("Total Employee wage is : " + totalWage);
-
-			} else {
-
-				System.out.println("Employee is absent ==>");
-				System.out.println("Total number of days Employee worked : " + countDay);
-				System.out.println("Total number of hours Employee worked : " + totalHrs);
-				System.out.println("Total Employee wage is :" + totalWage);
-
-			}
-
-			countDay++;
-			System.out.println();
-		}
+	public String getName() {
+		return name;
 	}
 
+	public void setEmpRate(final int empRate) {
+		this.empRate = empRate;
+	}
+
+	public int getEmpRate() {
+		return empRate;
+	}
+
+	public void setTotalWorkingDays(final int totalWorkingDays) {
+		this.totalWorkingDays = totalWorkingDays;
+	}
+
+	public int getTotalWorkingDays() {
+		return totalWorkingDays;
+	}
+
+	public void setMaxHrsInMonth(final int maxHrsInMonth) {
+		this.maxHrsInMonth = maxHrsInMonth;
+	}
+
+	public int getMaxHrsInMonth() {
+		return maxHrsInMonth;
+	}
 }
